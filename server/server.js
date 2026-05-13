f33cfaf2eb1bf312ea862db3078a8bdc4cf3a143
shriyaaditya <shriyadalai6@gmail.com>
2026-05-13 09:44:19 +0530

    fix: fixed CORS origin

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://intern-demo-6bcm.vercel.app/',
+  origin: process.env.CLIENT_URL || 'https://intern-demo-6bcm.vercel.app',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
