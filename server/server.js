50f3d7f9dcf8cefaa11470ae84447ae439b169bb
shriyaaditya <shriyadalai6@gmail.com>
2026-05-13 09:39:42 +0530

    feat: added production variables for frontend

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'https://intern-demo-6bcm.vercel.app/',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
